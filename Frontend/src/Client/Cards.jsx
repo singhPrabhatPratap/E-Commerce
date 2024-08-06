c8e9cb3f7a58fd27e6ae2454d2805eda3ab2992f
singhPrabhatPratap <prabhatbhibhu@gmail.com>
2024-08-06 15:26:45 +0530

    add

--- Frontend/src/Client/Cards.jsx
+++ Frontend/src/Client/Cards.jsx
@@ -85,7 +85,7 @@ export default function Cards() {
         <div
         className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
           {data.map((product) => (
-            <div className="rounded-md border" onClick={()=>showdetail(product)}>
+            <div className="rounded-md border shadow-xl" onClick={()=>showdetail(product)}>
               
               <img
                 src={`http://localhost:3000/${product.image}`}
