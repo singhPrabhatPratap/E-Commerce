054613df62206e7754eb70cf356afedcefecc431
prabhat <prabhatbhibhu@gmail.com>
2024-08-12 23:20:49 +0530

    add

--- Backend/Databaseconfig.js
+++ Backend/Databaseconfig.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql")
 let connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"1234",
-    database:"full"
+    host:process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE,
+    port:process.env.DB_PORT ||3306
 })
 module.exports = connection
